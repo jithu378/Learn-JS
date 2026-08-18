54230eee6facdf6302266f2e4ff8784bf63ed5b7
jithu378 <jithubiju378@gmail.com>
2026-08-18 14:35:49 +0530

    emplotee constructor

--- HomeTask/week-6/employeeConstructor.js
+++ HomeTask/week-6/employeeConstructor.js
@@ -11,3 +11,16 @@ function Employee(id, name, department, salary){
     this.salary = salary;
 }
 
+Employee.prototype.display = function(){
+    console.log(`ID : ${this.id}`);
+    console.log(`Name : ${this.name}`);
+    console.log(`Department : ${this.department}`);
+    console.log(`Salary : ${this.salary}`);
+    console.log("");
+}
+
+let employee1 = new Employee(1001,"Joel", "MERN", 47000);
+let employee2 = new Employee(1002,"Anandhavshnu","Logistics",39000);
+
+employee1.display();
+employee2.display();
