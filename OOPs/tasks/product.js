d08da578fb7abbb0ce0d23233805c1508f586cbf
jithu378 <jithubiju378@gmail.com>
2026-09-07 23:47:06 +0530

    product task

--- OOPs/tasks/product.js
+++ OOPs/tasks/product.js
@@ -1,3 +1,23 @@
+// 3. Create a Product class with:
+//  - Properties: id, name, price, stock
+//  - Method: updatePrice(newPrice) - validates price > 0
+//  - Method: updateStock(quantity) - adds or removes stock
+//  - Method: isAvailable() - returns true if stock > 0
+//  - Method: getValue() - returns total value (price * stock)
+
+//  Create a product and perform operations:
+//  1. Create a product (P101, "Laptop", 800, 10)
+//  2. Update price to 750
+//  3. Reduce stock by 3
+//  4. Check if available
+//  5. Display total value
+
+//  Expected Output:
+//  Price updated: 800 → 750
+//  Stock: 7
+//  Available: true
+//  Total value: 5250
+
 class Product {
     constructor(id, name, price, stock) {
         this.id = id;
@@ -9,6 +29,7 @@ class Product {
     updatePrice(newPrice) {
         if (newPrice > 0) {
             this.price = newPrice;
+            console.log(`Price updated : ${this.price}, for the product ${this.name}`)
         } else {
             console.log("Invalid price");
         }
@@ -16,6 +37,7 @@ class Product {
 
     updateStock(quantity) {
         this.stock += quantity;
+        console.log(`Stock updated: ${this.stock}`);
     }
 
     isAvailable() {
@@ -27,15 +49,8 @@ class Product {
     }
 }
 
-let product = new Product("P101", "Laptop", 800, 10);
-
-console.log(`Price: ${product.price}`);
-
-product.updatePrice(750);
-console.log(`Price updated: ${product.price}`);
-
-product.updateStock(-3);
-
-console.log("Stock:", product.stock);
-console.log("Available:", product.isAvailable());
-console.log("Total value:", product.getValue());
+let product1 = new Product("P101", "Laptop", 800, 10);
+product1.updateStock(10);
+product1.updatePrice(1000);
+console.log(`Product available : ${product1.isAvailable()}`);
+console.log(`Total value of the product : ${product1.getValue()}`);
