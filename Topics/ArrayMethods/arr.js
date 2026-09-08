3c959c2b012d4b91618bfe15ed725d2e671467be
jithu378 <jithubiju378@gmail.com>
2026-09-08 11:57:05 +0530

    changes

--- Topics/ArrayMethods/arr.js
+++ Topics/ArrayMethods/arr.js
@@ -1,1 +1,35 @@
-let arr
+// Array methods
+
+let arr = [23,5,89,0,10,11,3];
+
+//1. push() - insert new element at the end of the array
+arr.push(68);
+console.log(arr);
+
+//2. pop() - delete the last element of the array
+arr.pop(68);
+console.log(arr);
+
+//3. unshift() - insert new element at the starting of the array
+arr.unshift(68);
+console.log(arr);
+
+//4. shift() - insert new element at the starting of the array
+arr.shift(68);
+console.log(arr);
+ 
+arr.push(68);
+console.log(arr);
+ 
+arr.push(68);
+console.log(arr);
+ 
+arr.push(68);
+console.log(arr);
+ 
+arr.push(68);
+console.log(arr);
+ 
+arr.push(68);
+console.log(arr);
+ 
