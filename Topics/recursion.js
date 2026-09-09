1a2820a328004a36c88a707c24fafc0cbbd08963
jithu378 <jithubiju378@gmail.com>
2026-09-09 10:50:55 +0530

    recursion - sum of digits

--- Topics/recursion.js
+++ Topics/recursion.js
@@ -37,3 +37,12 @@
 // console.log(sum(5));
 
 // sum of digits eg: 123 = 6 that is 1+2+3
+
+// function sumOfDigits(n){
+//     if(n == 0){
+//         return 0;
+//     }
+//     return n%10 + sumOfDigits(Math.floor(n/10));
+// }
+// console.log(sumOfDigits(1230))
+
