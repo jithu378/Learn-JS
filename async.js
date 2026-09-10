ec4206aa4b76958208c65de4d19ef06127315bd3
jithu378 <jithubiju378@gmail.com>
2026-09-10 10:57:25 +0530

    async

---
+++ async.js
@@ -0,0 +1,10 @@
+// 1. File Upload
+//    Create: uploadFile(fileName, callback)
+//    Simulate a file upload that takes 5 seconds.
+//    While uploading:
+//    Uploading profile.jpg...
+//    You can continue using the application.
+//    After uploading:
+//    File uploaded successfully!
+//    Then the callback should display:
+//    Updating profile...
