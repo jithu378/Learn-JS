7aee43aa6b52e4c9ac6933529645b375553ec0b3
jithu378 <jithubiju378@gmail.com>
2026-09-13 21:49:26 +0530

    opt program

--- async.js
+++ async.js
@@ -8,3 +8,39 @@
 //    File uploaded successfully!
 //    Then the callback should display:
 //    Updating profile...
+
+function uploadFile(fileName, callback){
+    callback()
+}
+
+// 2. OTP Verification
+    // Create: sendOTP(phoneNumber, callback)
+    // Requirements:
+    // Print "Sending OTP to 9876543210..."
+    // Wait 3 seconds.
+    // Generate a random 4-digit OTP.
+    // Print "OTP sent successfully!"
+    // Pass the generated OTP to the callback.
+    // Then create:
+    // verifyOTP(otp)
+    // which prints:
+    // OTP received: 4832
+    // Please enter the OTP to continue.
+    // Additional requirement
+    // While the OTP is being sent:
+    // You can continue browsing the website...
+    // should be printed immediately.
+
+function sendOTP(phoneNumber,callback){
+    console.log(`Sending OTP to ${phoneNumber}`);
+    let otp = Math.floor(Math.random()*10000);
+    setTimeout(()=>{console.log("OPT sent successfully");
+        callback(otp)},3000);
+    console.log("You can browse the application");
+}
+
+function verifyOTP(opt){
+    console.log(`OTP received : ${opt}`);
+}
+
+sendOTP(9876543210,verifyOTP);
